728ab630cc36b78c507c2093ef612339411e5069
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 17:42:46 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -19,7 +19,9 @@ class Person
   end
   
   class PartnerError < StandardError 
-    "you must give the get_married method an argument of an instance of the person class!"
+    def message 
+      "you must give the get_married method an argument of an instance of the person class!"
+    end 
   end 
   
 end
